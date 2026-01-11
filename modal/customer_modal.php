854401f73d85660671b790794921a350846070f1
Rakib Mahmud <fiverrrakib2017@gmail.com>
2026-01-11 20:55:38 +0600

    customer script completed

--- modal/customer_modal.php
+++ modal/customer_modal.php
@@ -151,9 +151,182 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary">Create Customer</button>
+                        <button type="submit" class="btn btn-primary">Update Customer</button>
                     </div>
                 </form>
             </div>
         </div>
     </div> 
+
+
+     <script type="text/javascript">
+        
+        $('#addCustomerModal form').submit(function(e) {
+                e.preventDefault();
+                /*Get the submit button*/
+                var submitBtn = $('#addCustomerModal form').find('button[type="submit"]');
+
+                /* Save the original button text*/
+                var originalBtnText = submitBtn.html();
+
+                /*Change button text to loading state*/
+                submitBtn.html(
+                    `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><span class="visually-hidden">Loading...</span>`
+                );
+                var form = $(this);
+                var url = form.attr('action');
+                var formData = form.serialize();
+                $.ajax({
+                    type: 'POST',
+                    'url': url,
+                    data: formData,
+                    dataType: 'json',
+                    success: function(response) {
+                        if (response.success) {
+                            $('#addCustomerModal').modal('hide');
+                            toastr.success(response.message);
+                            setTimeout(() => {
+                                location.reload();
+                            }, 500);
+                        } else {
+                            toastr.error(response.message);
+                        }
+                    },
+
+
+                    error: function(xhr, status, error) {
+                        /** Handle  errors **/
+                        if (xhr.status === 422) {
+                            var errors = xhr.responseJSON.errors;
+                            $.each(errors, function(key, value) {
+                                toastr.error(value[0]);
+                            });
+                        }
+                    },
+                    complete: function() {
+                        submitBtn.html(originalBtnText);
+                    }
+                });
+            });
+             /** Edit Area Script **/
+            $(document).on('click', "button[name='edit_button']", function() {
+                var id = $(this).data('id');
+                $.ajax({
+                    url: "include/customer_server.php?get_customer_data=true",
+                    type: "GET",
+                    data: {
+                        id: id
+                    },
+                    dataType: 'json',
+                    success: function(response) {
+                        if (response.success) {
+                            $('#editCustomerModal').modal('show');
+                            $('#editCustomerModal input[name="id"]').val(response.data.id);
+                            $('#editCustomerModal input[name="customer_name"]').val(response.data.customer_name);
+                            $('#editCustomerModal input[name="customer_email"]').val(response.data.customer_email);
+                            $('#editCustomerModal input[name="customer_phone"]').val(response.data.customer_phone);
+                            $('#editCustomerModal input[name="customer_location"]').val(response.data.customer_location);
+                            $('#editCustomerModal select[name="customer_type"]').val(response.data.customer_type_id);
+                            $('#editCustomerModal input[name="customer_ip"]').val(response.data.customer_ip);
+                            $('#editCustomerModal input[name="customer_bandwidth"]').val(response.data.customer_bandwidth);
+                            $('#editCustomerModal select[name="customer_status"]').val(response.data.status);
+
+                        } else {
+                            toastr.error("Error fetching data for edit: " + response.message);
+                        }
+                    },
+                    error: function(xhr, status, error) {
+                        toastr.error('Failed to fetch  details');
+                    }
+                });
+            });
+            /** Update **/
+            $('#editCustomerModal form').submit(function(e) {
+                e.preventDefault();
+
+                var form = $(this);
+                var url = form.attr('action');
+                var formData = form.serialize();
+
+                /*Get the submit button*/
+                var submitBtn = form.find('button[type="submit"]');
+
+                /*Save the original button text*/
+                var originalBtnText = submitBtn.html();
+
+                /*Change button text to loading state*/
+
+
+                var form = $(this);
+                var url = form.attr('action');
+                var formData = form.serialize();
+                /** Use Ajax to send the delete request **/
+                $.ajax({
+                    type: 'POST',
+                    'url': url,
+                    data: formData,
+                    dataType: 'json',
+                    beforeSend: function() {
+                        form.find(':input').prop('disabled', true);
+                    },
+                    success: function(response) {
+                        if (response.success) {
+                             $('#editCustomerModal').modal('hide');
+                            toastr.success(response.message);
+                            setTimeout(() => {
+                                location.reload();
+                            }, 500);
+
+                        }
+                        if (response.success == false) {
+                            toastr.error(response.message);
+                        }
+                    },
+
+                    error: function(xhr, status, error) {
+                        if (xhr.status === 422) {
+                            var errors = xhr.responseJSON.errors;
+                            $.each(errors, function(key, value) {
+                                toastr.error(value[0]);
+                            });
+                        } else {
+                            toastr.error("An error occurred. Please try again.");
+                        }
+                    },
+                    complete: function() {
+                        form.find(':input').prop('disabled', false);
+                    }
+                });
+            });
+            /** Delete Script **/
+            $(document).on('click', "button[name='delete_button']", function() {
+                var id = $(this).data('id');
+                $('#DeleteId').text(id);
+                $('#deleteModal').modal('show');
+
+                $('#DeleteConfirm').off('click').on('click', function() {
+                    $.ajax({
+                        url: "include/customer_server.php?delete_customer_data=true",
+                        type: "POST",
+                        data: {
+                            id: id
+                        },
+                        dataType: 'json',
+                        success: function(response) {
+                            if (response.success) {
+                                toastr.success(response.message);
+                                $('#deleteModal').modal('hide');
+                                setTimeout(() => {
+                                    location.reload();
+                                }, 500);
+                            } else {
+                                toastr.error(response.message);
+                            }
+                        },
+                        error: function(xhr, status, error) {
+                            toastr.error("Error deleting NAS: " + xhr.responseText);
+                        }
+                    });
+                });
+            });
+    </script>
